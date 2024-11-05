f292960553ea8248f97b2536ce63ab21bff07240
TamerC <kuluzuk1@outlook.com>
2024-11-06 01:13:41 +0300

    css-duzenlendi

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import './index.css'
 const boyutlar = ['Küçük', 'Orta', 'Büyük']; 
-
 const hamurlar = ['İnce', 'Orta', 'Kalın'];
 function Form() {
     return (
         <div className='display-flex justify-content align-items text-content gap-10rem'>
             <div className="font-barlow color-black max-width">
-                <h2>Position Absolute Acı Pizza</h2>
-                <h1>85.50₺</h1>
-                <p className='text-align-start'>
-                    Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. Küçük bir pizzaya bazen pizzetta denir.
-                </p>
-                <div className='display-flex justify-content-start '>{/* Boyut - Hamur */}
+                <div className='display-flex flex-direction-column'>
+                    <h2>Position Absolute Acı Pizza</h2>
+                    <div className='display-flex flex-direction-row justify-content-sb'>
+                        <h1>85.50₺</h1>
+                        <div className='display-flex width-100 justify-content-end gap-3rem'>
+                            <p>4.9</p>
+                            <p>(200)</p>
+                        </div>
+                    </div>
+                    <p className='text-align-start'>
+                        Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. Küçük bir pizzaya bazen pizzetta denir.
+                    </p>
+                </div>
+                <div className='display-flex gap-9rem'>{/* Boyut - Hamur */}
                     <div>{/* Boyut Seç */}
                         <h2>Boyut Seç<span className='color-star'>*</span></h2>
                         {boyutlar.map((boyut, ind) => {
